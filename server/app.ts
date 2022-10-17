9c4febe16c12f7ee8be35b6692c487f3aec0b82a
matt <matthieu.l.dubray@gmail.com>
2022-10-17 12:43:41 -0700

    import mongoose (mongoDB ORM tool) setup logic to connect to DB (conn string in a .env file) created temp user model (will be refactored as more models and logic are added)

--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,22 @@
 import express from "express";
+import "dotenv/config";
+import { connect } from "mongoose";
 
 const app: express.Application = express();
-
 const port: number = 80;
+
+const connString: string = `${process.env.MONGODB_CONN_STRING}`;
 
 app.get("/", (_req, _res) => {
     _res.send("TypeScript With Express");
 });
 
-app.listen(port, () => {
-    console.log(`TypeScript with Express
-         http://localhost:${port}/`);
-});
+connect(connString)
+    .then((result) => {
+        app.listen(port, () => {
+            console.log(`node running on http://localhost:${port}/`);
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+    });
