9ee7535cc1ad11397235547accf1082be1f2b09d
matt <matthieu.l.dubray@gmail.com>
2022-10-19 18:40:42 -0700

    fixed issue where user would upload incorrect file types and server would keep files now server will delete the files if one of them is of an incorrect type

--- server/controllers/video.controller.ts
+++ server/controllers/video.controller.ts
@@ -1,19 +1,38 @@
 import { Request, Response } from "express";
 import Video from "../models/video/video";
 import { validationResult } from "express-validator";
+import path from "path";
+import { unlink } from "fs";
 
 export async function upload(_req: Request, _res: Response) {
     try {
+        let files;
+        if (_req.files) {
+            files = _req.files as {
+                [fieldname: string]: Express.Multer.File[];
+            };
+        }
         const errors = validationResult(_req);
         if (!errors.isEmpty()) {
+            if (files) {
+                deleteFile(files["video"][0].path);
+                deleteFile(files["thumbnail"][0].path);
+            }
             return _res.status(400).json({ errors: errors.array() });
         }
 
         console.log(_req.body);
         return _res.status(201).json({
-            message: "user created",
+            message: "video uploaded successfully",
         });
     } catch (err) {
         console.log(err);
     }
 }
+
+const deleteFile = async (imagePath: string) => {
+    let filePath = path.join(__dirname, "..", imagePath);
+    await unlink(filePath, (err) => {
+        if (err) console.log(err);
+    });
+};
--- server/routes/video.routes.ts
+++ server/routes/video.routes.ts
@@ -1,11 +1,36 @@
 import { Router } from "express";
-import { body, CustomValidator } from "express-validator";
+import { body, CustomValidator, check } from "express-validator";
 import { upload } from "../controllers/video.controller";
 
 import Video from "../models/video/video";
 
 const router = Router();
 
-router.put("/upload", upload);
+router.put(
+    "/upload",
+    body("title")
+        .isLength({ min: 5 })
+        .trim()
+        .withMessage("Title is too short, min: 5 characters"),
+    check("thumbnail")
+        .custom((value, { req }) => {
+            return (
+                req.files["thumbnail"][0].mimetype === "image/png" ||
+                req.files["thumbnail"][0].mimetype === "image/jpg" ||
+                req.files["thumbnail"][0].mimetype === "image/jpeg"
+            );
+        })
+        .withMessage(
+            "incorrect file type submitted (thumbnail must be a PNG, a JPG, or a JPEG)"
+        ),
+    check("video")
+        .custom((value, { req }) => {
+            return req.files["video"][0].mimetype === "video/mp4";
+        })
+        .withMessage(
+            "incorrect file type submitted (video must be an MP4 file)"
+        ),
+    upload
+);
 
 export default router;
